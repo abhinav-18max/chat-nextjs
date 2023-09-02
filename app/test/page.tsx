cb5d0844225c2ff4fc84a85747bf2e1dbdabfd73
abhinav <abhinav200218@gmail.com>
2023-09-03 02:07:35 +0530

    first commit

---
+++ app/test/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import { Calendar} from "@/components/ui/calendar";
+
+export default function Page() {
+    return(
+        <>
+        <h1>Test</h1>
+        <Calendar />
+        </>
+    )
+}
